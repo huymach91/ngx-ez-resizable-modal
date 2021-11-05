50d2b747e3c9b6f16772018916d88a97f005cb57
huymach91 <huy.mach.webdev@gmail.com>
2021-11-05 18:39:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-ez-resizable-modal
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-ez-resizable-modal)
---
+++ src/app/ngx-ez-resizable-modal/ngx-ez-resizable-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-ez-resizable-modal',
+  templateUrl: './ngx-ez-resizable-modal.component.html',
+  styleUrls: ['./ngx-ez-resizable-modal.component.css'],
+})
+export class NgxEzResizableModalComponent {}
